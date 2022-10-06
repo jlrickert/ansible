0d8ab0390397abacbc1851a18da0bb9625cef8ae
Jared Rickert <jaredrickert52@gmail.com>
2022-10-06 18:33:05 -0500

    feat: remove NVM zsh config
    Removing as this is now handled in my dotfiles

--- install_local.sh
+++ install_local.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR
 
-ansible-playbook local.yml -K --ask-vault-password -t "$@"
+ansible-playbook local.yml -K --ask-vault-password "$@
 
